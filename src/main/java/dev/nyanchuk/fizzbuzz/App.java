c2eed6fbec5757dfe4941e0ad4fd91d2325019c5
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-10-27 23:59:56 +0100

    feat: class FizzbuzzNumContain for etap 2

--- src/main/java/dev/nyanchuk/fizzbuzz/App.java
+++ src/main/java/dev/nyanchuk/fizzbuzz/App.java
@@ -5,8 +5,13 @@ public final class App {
     }
 
     public static void main(String[] args) {
+        // etap 1
+        //for (byte num = 1; num <= 100; num++) {
+        //    System.out.println(Fizzbuzz.FizzBuzzEvaluator1(num));
+        //}
+        // etap 2
         for (byte num = 1; num <= 100; num++) {
-            System.out.println(Fizzbuzz.FizzBuzzEvaluator1(num));
+            System.out.println(FizzbuzzNumContain.FizzBuzzEvaluator2(num));
         }
     }
     
---
+++ src/main/java/dev/nyanchuk/fizzbuzz/FizzbuzzNumContain.java
@@ -0,0 +1,31 @@
+package dev.nyanchuk.fizzbuzz;
+
+public class FizzbuzzNumContain {
+    byte number;
+
+    // constructor
+    FizzbuzzNumContain(byte num) {
+        this.number = num;
+    }
+
+    // function
+    public static String FizzBuzzEvaluator2(byte num) {   
+
+        if (num < 1 || num > 100) {
+            return "Use the range 1 to 100";
+        }
+        // this part is not in requirements but according to my logic has to be here.
+        //if (((num % 3 == 0) || String.valueOf(num).contains("3")) 
+        //    && ((num % 5 == 0) || String.valueOf(num).contains("5"))) {
+        //    return "FizzBuzz";
+        //}
+        if ((num % 3 == 0) || String.valueOf(num).contains("3")) {
+            return "Fizz";
+        }
+        if ((num % 5 == 0) || String.valueOf(num).contains("5")) {
+            return "Buzz";
+        }
+        return String.valueOf(num);
+    }
+    
+}
