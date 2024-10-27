a263e58afc6ae7c23b2489f55d961fa8c1da6ced
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-10-28 00:57:14 +0100

    ci: decomment calling  first etap class

--- src/main/java/dev/nyanchuk/fizzbuzz/App.java
+++ src/main/java/dev/nyanchuk/fizzbuzz/App.java
@@ -6,9 +6,9 @@ public final class App {
 
     public static void main(String[] args) {
         // etap 1
-        // for (byte num = 1; num <= 100; num++) {
-        // System.out.println(Fizzbuzz.FizzBuzzEvaluator1(num));
-        // }
+        for (byte num = 1; num <= 100; num++) {
+            System.out.println(Fizzbuzz.FizzBuzzEvaluator1(num));
+        }
         // etap 2
         for (byte num = 1; num <= 100; num++) {
             System.out.println(FizzbuzzNumContain.FizzBuzzEvaluator2(num));
